c049b551087971ff16f634e4d6c70a33c0c64a62
shaileshmhadaye <shaileshmhadaye2@gmail.com>
2019-09-16 15:02:47 +0530

    added Category entity and created ManyToMany relationship between category and recipe.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -29,6 +29,10 @@ public class Recipe {
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Set<Ingredient> getIngredients() {
         return ingredients;
     }
@@ -123,5 +127,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
